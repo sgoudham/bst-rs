fdf7d1c8cf27119b699acd570f43c89e7f1d07cc
Carsten Kragelund <carsten@kragelund.dev>
2022-10-01 02:35:39 +0200

    FIX(docs, tests): Fix empty doctests, and add docs
    * Adds documentation to the `bst!` macro
    
    * Changes the tree visualizations from `rust` blocks to just `text`, so
    that `cargo doc` does not complain
    
    * Runs `cargo fmt`

--- tests/macro.rs
+++ tests/macro.rs
@@ -1,17 +1,17 @@
-use bst_rs::bst;
+use bst_rs::{bst, BinarySearchTree, IterativeBST};
 
 #[test]
 fn successfully_construct_bst_from_macro() {
     let mut actual_bst = IterativeBST::new();
     actual_bst.insert(3);
     actual_bst.insert(2);
-    let expected_bst = bst![3,2];
+    let expected_bst = bst![3, 2];
     assert_eq!(actual_bst, expected_bst);
 }
 
 #[test]
 fn verify_permutations_produce_same_tree() {
-    let expected_bst = bst![2,3];
-    let expected_bst = bst![3,2];
+    let actual_bst = bst![2, 3];
+    let expected_bst = bst![3, 2];
     assert_eq!(actual_bst, expected_bst);
 }
